8ca243d7e30bd42ddeb0b415a491a2058416e708
davidjhurtado <davidjhurtado@gmail.com>
2021-05-17 16:21:02 -0400

    feat: Nuevas clases para el control de la tasa de cambio implementando MVVM

---
+++ DiviCalc/DiviCalc/Models/Cambio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiviCalc.Models {
+    public class Cambio {
+        public decimal TasaDeCambio { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public Cambio(decimal tasadeCambio, DateTime fecha) {
+            TasaDeCambio = tasadeCambio;
+            Fecha = fecha;
+        }
+    }
+}
---
+++ DiviCalc/DiviCalc/ViewModel/HomeViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiviCalc.ViewModel {
+    public class HomeViewModel:BaseViewModel {
+        private decimal tasaDeCambio = 0;
+        
+
+        public decimal TasaDeCambio {
+            get { return tasaDeCambio; }
+            set {
+                if (tasaDeCambio == value) {
+                    return;
+                }
+
+                tasaDeCambio = value;
+            }
+        }
+
+        public string Titulo {
+            get { return "DiviCal - Calculadora de divisas"; }
+            
+        }
+
+
+
+    }
+}
