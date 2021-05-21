50c27e33b67f89cc40625f5860121cf4e49becff
davidjhurtado <davidjhurtado@gmail.com>
2021-05-21 11:18:18 -0400

    fix: Ajuste del modelo cambio para que  pueda adapatarse al modelo e SQLIte

--- DiviCalc/DiviCalc/Models/Cambio.cs
+++ DiviCalc/DiviCalc/Models/Cambio.cs
@@ -1,15 +1,16 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace DiviCalc.Models {
-    public class Cambio {
+    [Table("Cambios")]
+    public  class Cambio {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
         public decimal TasaDeCambio { get; set; }
         public DateTime Fecha { get; set; }
 
-        public Cambio(decimal tasadeCambio, DateTime fecha) {
-            TasaDeCambio = tasadeCambio;
-            Fecha = fecha;
-        }
     }
 }
